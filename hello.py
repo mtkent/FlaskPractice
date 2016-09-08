e46e5d343a707e9e762e2578f9d0cba47f087036
Marina Kent <mtkent@hotmail.com>
2016-09-08 14:48:57 -0700

    Added basic python script
    hello world to debugger output

--- hello.py
+++ hello.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
+    return render_template('home.html')
+
+@app.route('/exec')
+def hello2():
+    import Hello2
+    print("done")
     return render_template('home.html')
 
 @app.route('/about/')
@@ -15,13 +20,6 @@ def about():
 def map():
     return render_template('map.html')
 
-@app.route('/exec')
-def parse(name=None):
-    import Parse
-    print("done")
-    return render_template('tour.html',name=name)
-
-
 if __name__ == '__main__':
-	    # app.run(host='0.0.0.0')
+	# app.run(host='0.0.0.0')
     app.run(debug=True)
